cf35a16f2d57b3579529a3fddb78682ab86f65f0
mikec <mikec@WIN11PRO-VM>
2022-08-30 21:00:22 -0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.IPText = new System.Windows.Forms.TextBox();
+            this.ScanPorts = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.portsLst = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.sportText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eportText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Enter an IP address to scan";
+            // 
+            // IPText
+            // 
+            this.IPText.Location = new System.Drawing.Point(15, 25);
+            this.IPText.Name = "IPText";
+            this.IPText.Size = new System.Drawing.Size(209, 20);
+            this.IPText.TabIndex = 1;
+            // 
+            // ScanPorts
+            // 
+            this.ScanPorts.Location = new System.Drawing.Point(16, 276);
+            this.ScanPorts.Name = "ScanPorts";
+            this.ScanPorts.Size = new System.Drawing.Size(75, 23);
+            this.ScanPorts.TabIndex = 2;
+            this.ScanPorts.Text = "Scan";
+            this.ScanPorts.UseVisualStyleBackColor = true;
+            this.ScanPorts.Click += new System.EventHandler(this.ScanPorts_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(150, 276);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 3;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // portsLst
+            // 
+            this.portsLst.FormattingEnabled = true;
+            this.portsLst.Location = new System.Drawing.Point(16, 97);
+            this.portsLst.Name = "portsLst";
+            this.portsLst.Size = new System.Drawing.Size(209, 173);
+            this.portsLst.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 305);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(209, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // sportText
+            // 
+            this.sportText.Location = new System.Drawing.Point(15, 71);
+            this.sportText.Name = "sportText";
+            this.sportText.Size = new System.Drawing.Size(58, 20);
+            this.sportText.TabIndex = 6;
+            this.sportText.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Start Port";
+            // 
+            // eportText
+            // 
+            this.eportText.Location = new System.Drawing.Point(169, 71);
+            this.eportText.Name = "eportText";
+            this.eportText.Size = new System.Drawing.Size(55, 20);
+            this.eportText.TabIndex = 8;
+            this.eportText.Text = "65535";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "End Port";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(237, 339);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.eportText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sportText);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.portsLst);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.ScanPorts);
+            this.Controls.Add(this.IPText);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IPText;
+        private System.Windows.Forms.Button ScanPorts;
+        private System.Windows.Forms.Button ClearBtn;
+        public System.Windows.Forms.ListBox portsLst;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox sportText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox eportText;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static int startPort;
+        static int endPort;
+
+        private List<int> openPorts = new List<int>();
+
+        private int maxQAtOnce = 3000;
+        private int awaitingResponses;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Start the scan. 
+        private async void ScanPorts_Click(object sender, EventArgs e)
+        {
+            portsLst.Items.Clear(); 
+            startPort = Convert.ToInt32(this.sportText.Text);
+            endPort = Convert.ToInt32(this.eportText.Text);
+            var progress = new Progress<int>(value =>
+            {
+                progressBar1.Value = value;
+            });
+            // Progress bar setup
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            //progressBar1.Value = 0;
+
+            // Init variable ipAddress
+            IPAddress ipAddress;
+
+            // TryParse the text into an IPAddress
+            if(!IPAddress.TryParse(IPText.Text, out ipAddress)) {
+                return; 
+            }
+
+            await Task.Run( () => StartScan(ipAddress, progress));
+            //ThreadPool.QueueUserWorkItem(StartScan, ipAddress);
+            
+        }
+
+        // Clear the textbox and the listbox.
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            IPText.Text = null;
+            portsLst.Items.Clear();
+            
+            
+        }
+
+        private void update_list_box(int openPort)
+        {
+            portsLst.BeginUpdate();
+            portsLst.Items.Add("Port " + openPort.ToString() + " Open!");
+            portsLst.EndUpdate();
+           
+        }
+
+        // Run the scan...
+        private void StartScan(object o, IProgress<int> progress)
+        {
+            IPAddress ipAddress = o as IPAddress;
+            
+            // From the startPort to the endPort
+            for (int i = startPort; i < endPort+1; i++)
+            {
+
+                while (awaitingResponses >= maxQAtOnce)
+                {
+                    Thread.Sleep(0);
+                }
+                
+                try
+                {
+                    // Create Socket
+                    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // Make connection
+                    s.BeginConnect(new IPEndPoint(ipAddress, i), EndConnect, s);
+                    progress.Report((i * 100) / endPort);
+                    IncrementResponses();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+        }
+
+
+        private void EndConnect(IAsyncResult ar)
+        {
+            
+            try
+            {
+                DecrementResponses();
+                Socket s = ar.AsyncState as Socket;
+                s.EndConnect(ar);
+
+                if (s.Connected)
+                {
+                    int openPort = Convert.ToInt32(s.RemoteEndPoint.ToString().Split(':')[1]);
+                    portsLst.BeginInvoke((Action)(() => {
+                        update_list_box(openPort);
+                    }));
+                    this.openPorts.Add(openPort);
+                    s.Disconnect(true);
+                    
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private void IncrementResponses()
+        {
+            Interlocked.Increment(ref awaitingResponses);
+        }
+        private void DecrementResponses()
+        {
+            Interlocked.Decrement(ref awaitingResponses);
+        }
+    }
+}
